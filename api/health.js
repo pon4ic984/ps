ccaa90606eeab937a8ce5b0057c93b0e54f4aa07
pon4ic984 <romanceuy1@gmail.com>
2025-12-23 22:17:12 +0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,3 +1,7 @@
 module.exports = (req, res) => {
-  res.status(200).json({ ok: true, ts: Date.now() });
+  res.status(200).json({
+    ok: true,
+    hasShopId: !!process.env.YOOKASSA_SHOP_ID,
+    hasSecret: !!process.env.YOOKASSA_SECRET_KEY
+  });
 };
